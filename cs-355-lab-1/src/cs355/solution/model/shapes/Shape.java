1049d9c0243a37df5f40dce85b1b06e03c224708
Ryan Cheatham <cheatham.ryan.t@gmail.com>
2014-02-17 16:27:53 -0700

    Removed AffineTransform mirror methods from Shape class

--- cs-355-lab-1/src/cs355/solution/model/shapes/Shape.java
+++ cs-355-lab-1/src/cs355/solution/model/shapes/Shape.java
@@ -1,7 +1,6 @@
 package cs355.solution.model.shapes;
 
 import java.awt.Color;
-import java.awt.geom.AffineTransform;
 
 import cs355.solution.controller.interfaces.Selectable;
 import cs355.solution.util.math.Matrix;
@@ -9,27 +8,19 @@ import cs355.solution.util.math.Vector2D;
 
 public abstract class Shape implements Selectable
 {
-	protected Color				color;
+	protected Color			color;
 
-	protected final Matrix		translation;
-	protected final Matrix		inverseTranslation;
-	protected final Matrix		rotation;
-	protected final Matrix		inverseRotation;
+	protected final Matrix	translation;
+	protected final Matrix	inverseTranslation;
+	protected final Matrix	rotation;
+	protected final Matrix	inverseRotation;
 
-	protected AffineTransform	translationJ;
-	protected AffineTransform	inverseTranslationJ;
-	protected AffineTransform	rotationJ;
-	protected AffineTransform	inverseRotationJ;
+	private Matrix			transform;
+	private Matrix			inverseTransform;
 
-	private Matrix				transform;
-	private Matrix				inverseTransform;
+	private float			rotationAngle;
 
-	private AffineTransform		transformJ;
-	private AffineTransform		inverseTransformJ;
-
-	private float				rotationAngle;
-
-	private boolean				dirty;
+	private boolean			dirty;
 
 	public Shape(Color color, Vector2D center)
 	{
@@ -38,11 +29,6 @@ public abstract class Shape implements Selectable
 		inverseTranslation = Matrix.getTranslateInstance(-center.x, -center.y);
 		rotation = new Matrix();
 		inverseRotation = new Matrix();
-
-		translationJ = AffineTransform.getTranslateInstance(center.x, center.y);
-		inverseTranslationJ = AffineTransform.getTranslateInstance(-center.x, -center.y);
-		rotationJ = new AffineTransform();
-		inverseRotationJ = new AffineTransform();
 
 		rotationAngle = 0;
 		dirty = true;
@@ -88,9 +74,6 @@ public abstract class Shape implements Selectable
 		this.rotation.rotate2D(dr);
 		inverseRotation.rotate2D(-dr);
 
-		rotationJ.rotate(dr);
-		inverseRotationJ.rotate(-dr);
-
 		dirty = true;
 	}
 
@@ -101,9 +84,6 @@ public abstract class Shape implements Selectable
 		this.rotation.rotate2D(rotation);
 		inverseRotation.rotate2D(-rotation);
 
-		rotationJ.rotate(rotation);
-		inverseRotationJ.rotate(rotation);
-
 		dirty = true;
 	}
 
@@ -111,9 +91,6 @@ public abstract class Shape implements Selectable
 	{
 		translation.translate2D(trans.x, trans.y);
 		inverseTranslation.translate2D(-trans.x, -trans.y);
-
-		translationJ.translate(trans.x, trans.y);
-		inverseTranslationJ.translate(-trans.x, -trans.y);
 
 		dirty = true;
 	}
@@ -161,12 +138,6 @@ public abstract class Shape implements Selectable
 
 		inverseTransform = new Matrix(inverseRotation);
 		inverseTransform.multiply(inverseTranslation);
-
-		transformJ = new AffineTransform(translationJ);
-		transformJ.concatenate(rotationJ);
-
-		inverseTransformJ = new AffineTransform(inverseRotationJ);
-		inverseTransformJ.concatenate(inverseTranslationJ);
 
 		dirty = false;
 	}
